8274d06929104689ef07631d5c48ac0ff38cbc56
hsiehyunju <hsiehyunju@gmail.com>
2023-06-16 15:08:49 +0800

    feat: 新增 shared 和 container

--- taipei-zoo/CoreDataManager.swift
+++ taipei-zoo/CoreDataManager.swift
@@ -9,4 +9,16 @@ import Foundation
 import CoreData
 
 class CoreDataManager {
+    
+    static let shared: CoreDataManager = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ZooDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 }
