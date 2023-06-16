b40cb2693f7c75d9da0c93f57271bc48d250b9ac
hsiehyunju <hsiehyunju@gmail.com>
2023-06-16 15:42:29 +0800

    feat: 新增取得植物資料

--- taipei-zoo/Services/TaipeiZooService.swift
+++ taipei-zoo/Services/TaipeiZooService.swift
@@ -38,4 +38,52 @@ class TaipeiZooService {
             }
         }.resume()
     }
+    
+    func getPlantData(completion: @escaping (Result<[PlantModel], Error>) -> Void) {
+        
+        var plantDataArray: [PlantModel] = [PlantModel]()
+        var maxDataCount: Int = 0
+        var isNext = true
+        
+        while(isNext) {
+            // 組合 API
+            var components = URLComponents()
+            components.scheme = "https"
+            components.host = "data.taipei"
+            components.path = "/api/v1/dataset/f18de02f-b6c9-47c0-8cda-50efad621c14"
+            
+            // 建立查詢參數
+            let scopeItem = URLQueryItem(name: "scope", value: "resourceAquire")
+            let resourceItem = URLQueryItem(name: "resource_id", value: "f18de02f-b6c9-47c0-8cda-50efad621c14")
+            let offsetItem = URLQueryItem(name: "offset", value: "\(plantDataArray.count)")
+            
+            // 建立伺服器請求
+            var request = URLRequest(url: components.url!)
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if error != nil {
+                    completion(.failure(error!))
+                }
+                
+                if let data = data {
+                    do {
+                        // 將 json 轉為 UserModel
+                        let decoder = JSONDecoder()
+                        let model = try decoder.decode(PlantResultModel.self, from: data)
+                        maxDataCount = model.result.count
+                        plantDataArray += model.result.results
+                        
+                        isNext = plantDataArray.count < maxDataCount
+                        
+                    } catch let error {
+                        completion(.failure(error))
+                    }
+                }
+                
+            }.resume()
+        }
+        completion(.success(plantDataArray))
+    }
 }
