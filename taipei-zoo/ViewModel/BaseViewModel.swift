f8463a9a9784add6dcc850300370a2cceaa418f9
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 21:36:48 +0800

    feat: 新增 Base View Model

---
+++ taipei-zoo/ViewModel/BaseViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewModel.swift
+//  taipei-zoo
+//
+//  Created by HSIEH YUN JU on 2023/6/17.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import CoreData
+
+class BaseViewModel: NSObject {
+    public var disposeBag = DisposeBag()
+}
