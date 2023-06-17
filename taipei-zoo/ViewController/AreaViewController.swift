297e7bc4cab3f53999d1598da4810220a6bbba4b
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 17:10:59 +0800

    test: 測試圖片下載

--- taipei-zoo/ViewController/AreaViewController.swift
+++ taipei-zoo/ViewController/AreaViewController.swift
@@ -34,6 +34,38 @@ class AreaViewController: UIViewController {
             
             cell.areaName?.text = model.name
             
+            let url = model.picURL.replacingOccurrences(of: "http", with: "https")
+            guard let imageURL = URL(string: url) else { return }
+            
+            let imageObservable = URLSession.shared.rx.data(request: URLRequest(url: imageURL))
+                        .map { data -> UIImage? in
+                            return UIImage(data: data)
+                        }
+                        .observe(on: MainScheduler.instance) // 確保更新UI的程式碼在主線程運行
+
+                    // 將下載的圖片綁定到imageView
+                    imageObservable
+                .bind(to: cell.areaImage.rx.image)
+                .disposed(by: self.disposeBag)
+            
+/*
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Error downloading image: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        cell.areaImage.image = image
+                    }
+                }
+            }
+
+            task.resume()
+             
+*/
+            
         }.disposed(by: disposeBag)
     }
 }
