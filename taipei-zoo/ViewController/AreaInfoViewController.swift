201767b873e69ca2ecd9c3260e438fdf5180c4d3
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 21:40:24 +0800

    feat: 新增 AreaInfoViewController

---
+++ taipei-zoo/ViewController/AreaInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AreaInfoViewController.swift
+//  taipei-zoo
+//
+//  Created by HSIEH YUN JU on 2023/6/17.
+//
+
+import Foundation
+import UIKit
+
+class AreaInfoViewController: UIViewController {
+    
+}
