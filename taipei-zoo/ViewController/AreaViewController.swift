9bbe881bbe35dc9f7a3c1f08d2c9c707e6aa3802
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 16:01:17 +0800

    feat: 添加綁定 UI

--- taipei-zoo/ViewController/AreaViewController.swift
+++ taipei-zoo/ViewController/AreaViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class AreaViewController: UIViewController {
     
+    
+    @IBOutlet var areaTableView: UITableView!
+    
+    private var viewModel = AreaViewModel()
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bindTableData()
+        viewModel.fetchData()
     }
     
-    
+    func bindTableData() {
+        
+        areaTableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        viewModel.items.bind(
+            to: areaTableView.rx.items(
+                cellIdentifier: "AreaTableViewCell", cellType: AreaTableViewCell.self)
+        ) { row, model, cell in
+            
+            cell.areaName?.text = model.name
+            
+        }.disposed(by: disposeBag)
+    }
 }
+
+extension AreaViewController: UITableViewDelegate {}
