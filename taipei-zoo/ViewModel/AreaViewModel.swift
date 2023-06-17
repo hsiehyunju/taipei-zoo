de8bbef4f4230173b225011f115ca8711043f7dc
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 09:20:51 +0800

    feat: 新增從 Core Data 提取資料的 function

--- taipei-zoo/ViewModel/AreaViewModel.swift
+++ taipei-zoo/ViewModel/AreaViewModel.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class AreaViewModel {
     
+    var items = PublishSubject<[AreaModel]>()
+    
+    func fetchData() {
+        if let areaArray = CoreDataManager.shared.fetchAllData(entityType: AreaEntity.self) {
+            var array = [AreaModel]()
+            for area in areaArray {
+                var model = AreaModel(id: Int(area.nid!)!, importdate: Importdate(date: "", timezoneType: 0, timezone: ""), no: area.no!, category: area.category!, name: area.name!, picURL: area.picURL!, info: area.info!, memo: "", geo: "", URL: area.url!)
+                array.append(model)
+            }
+            items.onNext(array)
+            items.onCompleted()
+        }
+    }
 }
