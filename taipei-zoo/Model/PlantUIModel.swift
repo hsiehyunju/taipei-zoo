50acb2cb9e0cb61e4b5bf751bfa1c60cef599602
hsiehyunju <hsiehyunju@gmail.com>
2023-06-17 21:39:42 +0800

    feat: 新增 PlantUIModel 用來給 UI 顯示用

---
+++ taipei-zoo/Model/PlantUIModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlantUIModel.swift
+//  taipei-zoo
+//
+//  Created by HSIEH YUN JU on 2023/6/17.
+//
+
+import Foundation
+
+struct PlantUIModel {
+    let no: String
+    let name: String
+}
