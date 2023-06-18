16e4e6d007729d7ad2a9189bbcc6cae41d3018fd
hsiehyunju <hsiehyunju@gmail.com>
2023-06-18 12:03:24 +0800

    feat: UI 製作設定

--- taipei-zoo/ViewController/AreaInfoViewController.swift
+++ taipei-zoo/ViewController/AreaInfoViewController.swift
@@ -14,6 +14,55 @@ class AreaInfoViewController: UIViewController {
     var viewModel: AreaInfoViewModel!
     private var disposeBag = DisposeBag()
     
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var areaIntro: UITextView!
+    @IBOutlet var plantTableView: UITableView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindUI()
+        
+        viewModel.plantModelArray.bind(
+            to: plantTableView.rx.items(
+                cellIdentifier: "PlantTableViewCell", cellType: PlantTableViewCell.self)
+        ) { _, model, cell in
+            cell.plantModel = model
+            cell.setupUI()
+        }.disposed(by: disposeBag)
+        
+        viewModel.fetchData()
+    }
+    
+    func bindUI() {
+        
+        self.titleLabel.text = viewModel.getName()
+        self.areaIntro.text = viewModel.getIntro()
+        
+        let imageObservable = URLSession.shared.rx.response(request: URLRequest(url: viewModel.getImageURL()))
+            .map{ response, data -> UIImage? in
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    return UIImage(systemName: "photo.fill")
+                }
+                
+                print("status code = \(httpResponse.statusCode)")
+                
+                if httpResponse.statusCode == 404 {
+                    print("enter")
+                    return UIImage(systemName: "photo.fill")
+                }
+                
+                var image = UIImage(data: data)
+                image = image?.resize(to: CGSize(width: self.imageView.bounds.width, height: self.imageView.bounds.height))
+                return image
+            }.observe(on: MainScheduler.instance)
+        
+        // 將下載的圖片綁定到imageView
+        imageObservable
+            .bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+                
+    }
 }
